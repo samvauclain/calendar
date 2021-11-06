4911784bd864013afbb9742a9dac23411f78b151
Sam Place <samvauclain@gmail.com>
2021-11-06 13:06:14 -0700

    in process of building calendar rows dynamically

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// WHEN I open the planner
-// - THEN the current day is displayed at the top of the calendar
-
-//  - HTML ID = currentDay
 var currentDayEl = $("#currentDay");
 
 // grab current day "moment()" and display in this format using moment: Saturday, Nov 06, 2021
@@ -11,5 +7,27 @@ var saveBtn = $('.saveBtn');
 currentDayEl.text(currentDay);
 
 // format current hour (example: 11)
-console.log("current hour:",moment().format("k"));
+console.log("current hour:", moment().format("k"));
+
+var calendarEntries = {};
+
+// container
+/* <div class="row">
+<div class="col-2 col-md-1 hour">
+    <span></span>
+</div>
+<textarea class="col-8 col-md-10 past form-control"></textarea>
+<div class="col-2 col-md-1 saveBtn">
+  <p><i class="fas fa-save"></i></p>
+</div>
+</div> */
+
+// On page load, dynamically create the calender rows & columns
+$(document).ready(function() {
+    // create elements for calendar entry              
+    var rowEl = $("<div></div>").addClass("row");
+    var colEl = $("<div></div>").addClass("col-2 col-md-1 hour");
+    colEl.append("<span></span>")
+    $(".container").append(rowEl, colEl);
+});
 
